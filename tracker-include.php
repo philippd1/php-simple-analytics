3eb515cf16b267bfe470812609647b4ef4c2030b
Philipp Dormann <ssspammail111@gmail.com>
2019-02-11 20:31:02 +0100

    add DB

--- tracker-include.php
+++ tracker-include.php
@@ -1,6 +1,9 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 class PHPSimpleTracker {
     var $installationDir = '';
+    var $basedir = '';
+    var $pathArray = array();
     function setInstallationDir($dirname){
         $this->installationDir = $dirname;
     }
@@ -11,7 +14,31 @@ class PHPSimpleTracker {
         $track = $_SERVER['PHP_SELF'];
         $track = str_replace($this->installationDir, '', $track);
         $timestamp = $this->getTimestamp();
+        
+        $this->pathArray = explode('/', $track);
+        $this->basedir = $this->pathArray[0];
+        
+        echo $this->basedir;
+        
+        $this->saveToDB($track, $timestamp);
     }
-}
-$tracker = new PHPSimpleTracker();
-?>
+    function saveToDB($trackPage, $timestamp){
+        if($this->basedir){
+            
+        }
+        $database = new \Filebase\Database([
+            'dir' => 'db/',
+            'cache' => true,
+            ]);
+            
+            $item = $database->get('phpanalytics');
+            $item->id = uniqid('123456789');
+            $item->trackPage = $trackPage;
+            $item->timestamp  = $timestamp;
+            $item->save();            
+            // if ($database->has('phpanalytics')){
+                // }
+            }
+        }
+        $tracker = new PHPSimpleTracker();
+        ?>
